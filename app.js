a7175a75532cb025be07fb21ece5a28c2fd2ab1a
mohamedammar2729 <mo.bas.ammar@el-eng.menofia.edu.eg>
2025-03-13 15:39:19 +0200

    Server update V1.2 to fix some errors

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const cors = require("cors");
 
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const path = require("path");
 
 /* ======================================================================================= */
 
