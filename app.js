d71b2e9cc6b6c1a29373cb3dc952f417139c039f
mohamedammar2729 <mo.bas.ammar@el-eng.menofia.edu.eg>
2025-03-13 16:16:34 +0200

    Server update V1.5 to fix some bugs

--- app.js
+++ app.js
@@ -42,9 +42,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(
   cors({
-    origin:
-      "http://localhost:3000/" ||
-      "https://iti-server-production.up.railway.app", // Replace with your frontend URL
+    origin: [
+      process.env.FRONTEND_URL ||
+      "http://localhost:3000",
+    ],
     credentials: true,
   })
 );
