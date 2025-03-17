a61f9c414f7470a506451f6462702a6cc5cd33c1
mohamedammar2729 <mo.bas.ammar@el-eng.menofia.edu.eg>
2025-03-17 19:12:57 +0200

    update

--- app.js
+++ app.js
@@ -40,10 +40,9 @@ const port = process.env.PORT || 3000;
 // middleware use to parse the body of the request and store it in req.body
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// "https://mohamedammar2729.github.io"||
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL || "http://localhost:3000"],
+    origin: [process.env.FRONTEND_URL || "https://mohamedammar2729.github.io"],
     credentials: true,
   })
 );
